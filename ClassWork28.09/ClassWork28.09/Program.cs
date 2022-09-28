13bc792a395d0c3e881d321a257a01f5f4f3286f
wudisluv <vlad.kornilov2015@gmail.com>
2022-09-28 11:01:16 +0300

    Text

---
+++ ClassWork28.09/ClassWork28.09/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRead
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextRead();
+            Console.ReadLine();
+        }
+        public static async void TextRead()
+        {
+            using (StreamReader stream = new StreamReader("text.txt"))
+            {
+                string text = await stream.ReadToEndAsync();
+                Console.WriteLine(text);
+            }
+        } 
+    }
+}
